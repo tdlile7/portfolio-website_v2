6a75b06cbb50d9df02f3fe5c1ed189fc7425eed3
tlilecode <tlilecode@gmail.com>
2019-06-06 11:08:35 -0700

    Update responsive styles of paragraph on card1 component.

--- src/components/card1.jsx
+++ src/components/card1.jsx
@@ -7,21 +7,27 @@ const Card1 = () => {
       <p>
         I am a highly motivated and self-driven individual who is seeking their
         first opportunity to enter the software development space. The
-        continuous innovation of software technologies, in addition to the easy
+        continuous innovation of software technologies, in addition to the ease
         of accessibility and effect of software applications is what makes the
-        prospect of being apart of this field so incredibly exciting. Most of my
-        knowledge and experience revolves around utilizing the MERN stack for
-        web application development. My preference would be to work as a
-        full-stack developer, to not only utilize the full-scale of my knowledge
-        but also to contribute and be apart of both sides of application
-        development. With that said, I am looking to join teams that can offer
-        mentorship where I could receive feedback that would ultimately help
-        improve and refine my own coding practices and skills. If you wish to
-        get a hold of me, please email me
+        prospect of being apart of this field so incredibly exciting.
+        <br />
+        <br />
+        Most of my knowledge and experience revolves around utilizing the MERN
+        stack for web application development. My preference would be to work as
+        a full-stack developer, to not only utilize the full-scale of my
+        knowledge, but also to contribute and be apart of both sides of
+        application development.
+        <br />
+        <br />
+        With that said, I am looking to join teams that can offer mentorship
+        where I could receive feedback that would ultimately help improve and
+        refine my own coding practices and skills.
+        <br />
+        <br />
+        If you wish to get a hold of me, please email me
         <a href="mailto:tlile1429@gmail.com"> @tlile1429@gmail.com</a> or via
-        the
-        <a href="#contactMe"> contact form</a> below. I look forward to hearing
-        from you soon!
+        the <a href="#contactMe"> contact form</a> below. I look forward to
+        hearing from you soon!
       </p>
     </>
   );
